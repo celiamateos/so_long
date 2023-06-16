9274c15da9830bfa0b4f90c3b936074deff9f7c0
Celia Mateos Martin <cmateos-@c1r10s5.42madrid.com>
2023-06-16 17:36:09 +0200

    ok

--- src/ft_check_path.c
+++ src/ft_check_path.c
@@ -6,33 +6,66 @@
 /*   By: cmateos <cmateos-@student.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 18:32:20 by cmateos           #+#    #+#             */
-/*   Updated: 2023/06/14 18:32:56 by cmateos-         ###   ########.fr       */
+/*   Updated: 2023/06/16 14:48:14 by cmateos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-# include "../so_long.h"
+#include "../so_long.h"
+
+void	ft_player_position(t_list *e)
+{
+	int	y;
+	int	x;
+
+	y = 0;
+	x = 0;
+	while (e->map[y] != NULL)
+	{
+		while (e->map[y][x] != '\0')
+		{
+			if (e->map[y][x] == 'P')
+			{
+				ft_check_path(e, y, x);
+				break ;
+			}
+			x++;
+		}
+		x = 0;
+		y++;
+	}
+}
 
 int	ft_check_path(t_list *e, int y, int x)
 {	
-	int valid;
-
-	valid = 0;
-	if (valid && (e->m[y + 1][x] == '0' || e->m[y - 1][x] == '0'
-		|| e->m[y][x + 1] == '0' || e->m[y + 1][x - 1] == '0'))
-		e->m[y][x] = '.';
-	if (valid && e->m[y + 1][x] == '0')
-		valid = ft_check_path(e, y + 1, x);
-	if (valid && e->m[y - 1][x] == '0')
-		valid = ft_check_path(e, y - 1, x);
-	if (valid && e->m[y][x + 1] == '0')
-		valid = ft_check_path(e, y, x + 1);
-	if (valid && e->m[y][x - 1] == '0')
-		valid = ft_check_path(e, y, x - 1);
-	e->m[y][x] = '.';
-	return (1);
+	if (e->count_collect == e->collectable && e->count_exit == 1)
+		return (1);
+	if (e->m[y][x] == 'C')
+	{
+		e->count_collect++;
+		e->m[y][x] = '0';
+	}
+	if (e->m[y][x] == 'E')
+	{
+		e->count_exit = 1;
+		e->m[y][x] = '0';
+	}
+	if (e->m[y + 1][x] != '1' || e->m[y - 1][x] != '1'
+		|| e->m[y][x + 1] != '1' || e->m[y][x - 1] != '1')
+		e->m[y][x] = '1';
+	if (e->m[y + 1][x] != '1')
+		ft_check_path(e, y + 1, x);
+	if (e->m[y - 1][x] != '1')
+		ft_check_path(e, y - 1, x);
+	if (e->m[y][x + 1] != '1')
+		ft_check_path(e, y, x + 1);
+	if (e->m[y][x - 1] != '1')
+		ft_check_path(e, y, x - 1);
+	e->m[y][x] = '1';
+	return (0);
 }
 
 void	ft_check_valid_path(t_list *e)
 {
-	//int ft_check_path(e, y, x);
-	printf("estamos en ello!");
+	ft_player_position(e);
+	if (e->count_collect != e->collectable || e->count_exit != 1)
+		ft_error(e, 7);
 }
